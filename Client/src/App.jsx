88c7014be2d52751c790f75275324d2a105376b2
Kevin Masson <kevin.j.masson@gmail.com>
2024-05-15 11:46:27 -0600

    Added some visual transition for the input

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,12 +7,13 @@ import Weather from './components/weather.jsx'
 function App() {
   const [location, setLocation] = useState('');
   const [weather, setWeather] = useState('');
+  const [inputWidth, setinputWidth] = useState("100vw");
 
   return (
     <div id ="app">
-    <div id="dataInput">
+    <div id ="dataInput" style={{width: inputWidth}}>
     <Input location={location} setLocation={setLocation}/>
-    <Submit location={location} setWeather={setWeather}/>
+    <Submit location={location} setWeather={setWeather} setinputWidth={setinputWidth}/>
     </div>
     <Weather weather={weather}/>
     </div>
--- Client/src/components/submit.jsx
+++ Client/src/components/submit.jsx
@@ -1,19 +1,20 @@
 import '../styles/submit.css';
 export default Submit;
 
-async function GetWeather({location, setWeather}) {
+async function GetWeather({location, setWeather, setinputWidth}) {
     try {
         const response = await fetch('http://127.0.0.1:3000/' + location, {
         });
         const data = await response.json();
         setWeather(JSON.stringify(data));
+        setinputWidth("30vw");
     } catch (err) {
         console.log(err);
     }
 }
 
-function Submit ({location, setWeather}) {
+function Submit ({location, setWeather, setinputWidth}) {
     return (
-        <button onClick={() => GetWeather({location, setWeather})}>Submit</button>
+        <button onClick={() => GetWeather({location, setWeather, setinputWidth})}>Submit</button>
     )
 }
